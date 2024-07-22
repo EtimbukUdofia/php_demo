06842b2281aa85a2468d73bd7b50e73350bb64ba
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-07-22 10:50:01 +0100

    provide authorization for each note in db refactor status codes

--- controllers/note.php
+++ controllers/note.php
@@ -6,8 +6,16 @@ $db = new Database($config["database"]);
 $heading = "Note";
 
 $id = $_GET['id'];
+$currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', ["id" => $id])->fetch();
 
+if(!$note){
+  abort();
+}
+
+if($note["user_id"] != $currentUserId){
+  abort(Response::FORBIDDEN);
+}
 
 require './views/note.view.php';
