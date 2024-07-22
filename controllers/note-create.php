62a3fefa5c025360caaa3c64aa8f7d0f916ea367
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-07-22 13:10:19 +0100

    add new notes to database

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,13 @@
 <?php
 
+$config = require 'config.php';
+$db = new Database($config["database"]);
+
 $heading = "Create Note";
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+  $body = $_POST["body"];
+  $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', ["body"=>$body, "user_id"=>1]);
+}
 
 require "views/note-create.view.php";
