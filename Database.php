7bac993da90268f8d85469d75311146272a30f8a
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-07-22 13:17:00 +0100

    add comments to Database class for easy interpretation

--- Database.php
+++ Database.php
@@ -8,21 +8,21 @@ class Database
 
   public function __construct($config, $username = "root", $password = "")
   {
-    $dsn = "mysql:" . http_build_query($config, "", ";");
+    $dsn = "mysql:" . http_build_query($config, "", ";"); // http_build_query wiil convert its content ($config) into something like query parameters
 
     $this->connection = new PDO($dsn, $username, $password, [
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // this will make the default return from fetching to be an associative array
     ]);
   }
 
-  public function query($query, $params = [])
+  public function query($query, $params = []) // the query string should always point to anonymous values which will be given meaning to by the $params
   {
     $this->statement = $this->connection->prepare($query);
-    $this->statement->execute($params);
+    $this->statement->execute($params); // this will prevent sql injections from happening from bad urls
     return $this;
   }
 
-  public function find()
+  public function find() // custom find method
   {
     return $this->statement->fetch();
   }
