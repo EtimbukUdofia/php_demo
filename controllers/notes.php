355dae1dd7cf3457377c36937ab26d8c8ebf0df6
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-07-22 10:25:40 +0100

    add routing for each individual note by id from params in URI

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,4 +1,12 @@
 <?php
-$heading = "Notes";
+
+$config = require "./config.php";
+$db = new Database($config["database"]);
+
+$heading = "My Notes";
+
+$notes = $db->query('select * from notes where user_id = 1')->fetchAll();
+
+// dumpAndDIe($notes);
 
 require './views/notes.view.php';
--- views/note.view.php
+++ views/note.view.php
@@ -4,13 +4,12 @@
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <?php foreach ($notes as $note) : ?>
-      <li>
-        <a href="/note?id=<?= $note["id"] ?>" class="text-blue-900 hover:underline">
-          <?= $note["body"] ?>
-        </a>
-      </li>
-    <?php endforeach; ?>
+    <p class="mb-6">
+      <a href="/notes" class="text-blue-900 hover:underline">Go back ...</a>
+    </p>
+    <p>
+      <?= $note["body"] ?>
+    </p>
   </div>
 </main>
 <?php require "partials/foot.php" ?>
