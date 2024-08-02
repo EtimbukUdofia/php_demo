ae26b4f39209acebf6c4a1182ad318e9b918fc69
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-02 10:13:21 +0100

    update controller to handle delete request

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,14 +5,23 @@ use core\Database;
 $config = require base_path("./config.php");
 $db = new Database($config["database"]);
 
-$id = $_GET['id'];
 $currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', ["id" => $id])->findOrFail();
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+  $note = $db->query('select * from notes where id = :id', ["id" => $_GET['id']])->findOrFail();
+  authorize($note["user_id"] === $currentUserId);
 
-authorize($note["user_id"] === $currentUserId);
+  $db->query('delete from notes where id = :id', ["id" => $_POST["id"]]);
 
-view("notes/show.view.php", [
-  "heading" => "Note",
-  "note" => $note,
-]);
+  header("location: /notes"); // redirect
+  exit();
+}else{
+  $note = $db->query('select * from notes where id = :id', ["id" => $_GET['id']])->findOrFail();
+  authorize($note["user_id"] === $currentUserId);
+  
+  view("notes/show.view.php", [
+    "heading" => "Note",
+    "note" => $note,
+  ]);
+}
+
