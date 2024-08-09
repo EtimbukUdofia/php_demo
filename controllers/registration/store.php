dc646e8b3f44f02a40c2270312964125b74ee6f5
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-09 11:27:27 +0100

    add functionality to handle Middleware in the routes

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -35,7 +35,7 @@ $user = $db->query('select * from users where email = :email', [
 // if yes, redirect to login page
 if ($user) {
   // that user already exists
-  header("location: /");
+  header("location: /"); //redirect to login page
   exit();
 } else {
   // if no, save one to db and then log the user in and redirect
@@ -52,34 +52,3 @@ if ($user) {
   header("location: /");
   exit();
 }
-
-
-
-
-
-
-// use core\App;
-// use core\Database;
-// use core\Validator;
-
-// require base_path('core/Validator.php');
-
-// $db = App::resolve(Database::class);
-
-// $errors = [];
-
-// if (!Validator::string($_POST["body"], 1, 500)) {
-//   $errors["body"] = "A body of no more than 500 characters is required";
-// }
-
-// if (!empty($errors)) {
-//   return view("notes/create.view.php", [
-//     "heading" => "Create Note",
-//     "errors" => $errors,
-//   ]);
-// }
-
-// $body = $_POST["body"];
-// $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', ["body" => $body, "user_id" => 1]);
-// header("location: /notes");
-// die();
